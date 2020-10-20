a170e78a2a55c4d25194fe280de10f758af5fdc1
matancarmeli7 <45543087+matancarmeli7@users.noreply.github.com>
2020-10-21 00:03:52 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-docker build -t bad_test -f bad_dockerfile_test
-docker build -t good_test -f dockerfile_test
+docker build -t bad_test -f bad_dockerfile_test .
+docker build -t good_test -f dockerfile_test .
