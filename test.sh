aa130e2b99c80c65a47631dfcb249f60c35b9c36
matancarmeli7 <matan.carmeli7@gmail.com>
2021-05-10 12:15:56 +0300

    add csv_version parameter to docker run

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ export csv_version=`yq .channels[0].currentCSV upstream-community-operators/ibm-
 export csv_version=`echo ${csv_version//ibm-block-csi-operator.v}`
 export csv_version=`echo ${csv_version//\"}`
 export csv_file=~/work/$CI_REPOSITORY_NAME/$CI_REPOSITORY_NAME/upstream-community-operators/ibm-block-csi-operator-community/$csv_version/ibm-block-csi-operator.v$csv_version.clusterserviceversion.yaml
-export wanted_image=ibmcom/ibm-block-csi-operator:1.6.0
+export wanted_image=ibmcom/ibm-block-csi-operator:1.5.0
 export current_image=`yq .metadata.annotations.containerImage $csv_file`
 export current_image=`echo ${current_image//\"}`
 
@@ -17,4 +17,4 @@ ansible-pull -U https://github.com/operator-framework/operator-test-playbooks -C
 echo Running test
 docker run -d -i -e ANSIBLE_CONFIG=/playbooks/upstream/ansible.cfg -e GODEBUG=x509ignoreCN=0 --name op-test --privileged --net host -v ~/.optest/certs:/usr/share/pki/ca-trust-source/anchors -e STORAGE_DRIVER=vfs -e BUILDAH_FORMAT=docker -v ~/work/$CI_REPOSITORY_NAME/$CI_REPOSITORY_NAME:/tmp/community-operators-for-catalog quay.io/operator_testing/operator-test-playbooks:latest
 docker cp ~/.kube/ op-test:/root/
-docker exec -i -e ANSIBLE_CONFIG=/playbooks/upstream/ansible.cfg -e GODEBUG=x509ignoreCN=0 op-test /bin/bash -c 'update-ca-trust && ansible-playbook -i localhost, -e ansible_connection=local upstream/local.yml -e run_upstream=true -e image_protocol='\''docker://'\'' -vv -e container_tool=podman -e run_prepare_catalog_repo_upstream=false -e operator_dir=/tmp/community-operators-for-catalog/upstream-community-operators/ibm-block-csi-operator-community -e operator_version=1.5.0 --tags pure_test -e operator_channel_force=optest -e strict_mode=true -e opm_index_add_mode=replaces'
+docker exec -i -e ANSIBLE_CONFIG=/playbooks/upstream/ansible.cfg -e GODEBUG=x509ignoreCN=0 op-test /bin/bash -c 'update-ca-trust && ansible-playbook -i localhost, -e ansible_connection=local upstream/local.yml -e run_upstream=true -e image_protocol='\''docker://'\'' -vv -e container_tool=podman -e run_prepare_catalog_repo_upstream=false -e operator_dir=/tmp/community-operators-for-catalog/upstream-community-operators/ibm-block-csi-operator-community -e operator_version=$csv_version --tags pure_test -e operator_channel_force=optest -e strict_mode=true -e opm_index_add_mode=replaces'
