3affda85aab6f5efe946208c1179c178425ab2a9
matancarmeli7 <45543087+matancarmeli7@users.noreply.github.com>
2021-05-12 15:12:23 +0300

    Update update_csv.sh

--- build/ci/update_csv.sh
+++ build/ci/update_csv.sh
@@ -2,3 +2,10 @@
 set +o pipefail
 
 sed -i "s+$current_image+$wanted_image+g" $csv_file
+sed -i 's/.*minKubeVersion.*/  minKubeVersion: 1.19.0/' $csv_file
+
+mkdir $repository_path/upstream-community-operators/
+mkdir $repository_path/community-operators/
+
+cp -r $repository_path/deploy/olm-catalog/ibm-block-csi-operator-community/ $repository_path/upstream-community-operators/
+cp -r $repository_path/deploy/olm-catalog/ibm-block-csi-operator-community/ $repository_path/community-operators/
